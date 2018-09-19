446efc555defd5420cc0a328653fbb1debdc6545
Juned Alam <juned5252@gmail.com>
2018-09-19 13:25:50 -0400

    cleaned commonapi and added other page classes for Expedia

--- Amazon/src/test/java/search/TestSearch.java
+++ Amazon/src/test/java/search/TestSearch.java
@@ -1,20 +1,21 @@
-package search;
-
-import base.CommonAPI;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
-import pages.SearchPage;
-import reporting.TestLogger;
-import sun.jvm.hotspot.debugger.Page;
-
-import java.io.IOException;
-
-public class TestSearch extends CommonAPI {
-
-    @Test
-    public void searchData()throws IOException{
-        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        SearchPage searchPage = PageFactory.initElements(driver,SearchPage.class);
-        searchPage.searchItemsAndSubmitButton();
-    }
-}
+//package search;
+//
+//import base.CommonAPI;
+//import org.openqa.selenium.support.PageFactory;
+//import org.testng.annotations.Test;
+//import pages.SearchPage;
+//import reporting.TestLogger;
+//
+//import java.io.IOException;
+//
+//public class TestSearch extends CommonAPI {
+//
+//
+//
+//    @Test
+//    public void searchData()throws IOException{
+//        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+//        SearchPage searchPage = PageFactory.initElements(driver,SearchPage.class);
+//        searchPage.searchItemsAndSubmitButton();
+//    }
+//}
---
+++ Expedia/src/main/java/Pages/PageObject.java
@@ -0,0 +1,16 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageObject {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public PageObject(WebDriver driver){
+
+    }
+
+}

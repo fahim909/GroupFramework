7807b739ced89e8a14bedf684e891149a3f8b51c
Juned Alam <juned5252@gmail.com>
2018-09-26 22:57:16 -0400

    added phone validation test in Customerservice Page

--- Expedia/src/main/java/Pages/CustomerServicePage.java
+++ Expedia/src/main/java/Pages/CustomerServicePage.java
@@ -1,9 +1,11 @@
 package Pages;
 
 import base.CommonAPI;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class CustomerServicePage extends CommonAPI {
 
@@ -14,9 +16,25 @@ public class CustomerServicePage extends CommonAPI {
     @FindBy(xpath = "//h3[@class='aside-prompt']")
     WebElement CustomerServiceLabel;
 
+    @FindBy(css = "#mad-t2d-form-input")
+    WebElement phoneNumberInputBox;
+
+    @FindBy(xpath = "//button[@class = 'btn-secondary btn-sub-action']")
+    WebElement SendButton;
+
     public String CSLabelText(){
         String text = CustomerServiceLabel.getText();
         return text;
     }
+
+    public void verifyPhoneTextLabel() throws InterruptedException {
+        phoneNumberInputBox.clear();
+        phoneNumberInputBox.sendKeys("9172924196");
+        SendButton.click();
+        Thread.sleep(2000);
+        WebElement labelText = driver.findElement(By.xpath("//div[@class='alert-message']"));
+        Assert.assertEquals(labelText.getText(),"Thank you! A text with the download link has been sent to your mobile device");
+    }
+
 
 }
